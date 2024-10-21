3dde2b017e19154fb73794aaa4c59d11b4d6590a
«vvychelm» <wychelm@ya.ru>
2024-10-21 15:46:23 +0700

    Send for review

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/vvychelm/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+// DOM узлы
+const cardList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+// Функция создания карточки
+function createCard(card, removeCard) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    cardElement.querySelector('.card__title').textContent = card.name;
+    cardElement.querySelector('.card__image').src = card.link;
 
-// @todo: Функция удаления карточки
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', removeCard);
 
-// @todo: Вывести карточки на страницу
+    return cardElement;
+};
+
+// Функция удаления карточки
+function deleteCard(evt) {
+    evt.target.closest('.places__item').remove();
+};
+
+// Функция вывода карточек на страницу
+function renderCards(cards) {
+    cards.forEach(card => {
+        const element = createCard(card, deleteCard);
+        cardList.append(element);
+    });
+}
+
+renderCards(initialCards);
