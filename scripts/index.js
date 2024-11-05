c0b64f29aa457e27cef9f802d62e77c9d31216db
«vvychelm» <wychelm@ya.ru>
2024-11-05 23:37:48 +0700

    Correct the comments and send for review #2

--- scripts/index.js
+++ scripts/index.js
@@ -9,6 +9,7 @@ function createCard(card, removeCard) {
     const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
     cardElement.querySelector('.card__title').textContent = card.name;
     cardElement.querySelector('.card__image').src = card.link;
+    cardElement.querySelector('.card__image').alt = card.name;
 
     const deleteButton = cardElement.querySelector('.card__delete-button');
     deleteButton.addEventListener('click', removeCard);
